6344d77799e7b4bf8159ec63edf2a34ef18ba745
Jerome2840bw <Jerome.soh@bluewireless.com>
2026-02-20 13:49:51 +0800

    update icons with vite-plugin-svgr for colour change

--- src/components/Sidenavbar.tsx
+++ src/components/Sidenavbar.tsx
@@ -1,14 +1,15 @@
 import { NavLink } from 'react-router-dom';
 import bwLogo from '../assets/bw-logo-white.png';
-import overviewIcon from '../assets/rectangles-mixed.svg';
-import deviceMgtIcon from '../assets/laptop-mobile.svg';
-import analyticsIcon from '../assets/file-chart-column-svg.svg';
-import locationTrackingIcon from '../assets/location-crosshairs.svg';
-import billingIcon from '../assets/file-invoice.svg';
-import accountSettingsIcon from '../assets/screwdriver-wrench.svg';
-import supportIcon from '../assets/headset.svg';
-import userIcon from '../assets/user.svg';
-import logoutIcon from '../assets/right-from-bracket.svg';
+import overviewIcon from '../assets/rectangles-mixed.svg?react';
+import deviceMgtIcon from '../assets/laptop-mobile.svg?react';
+import analyticsIcon from '../assets/file-chart-column-svg.svg?react';
+import locationTrackingIcon from '../assets/location-crosshairs.svg?react';
+import billingIcon from '../assets/file-invoice.svg?react';
+import accountSettingsIcon from '../assets/screwdriver-wrench.svg?react';
+import supportIcon from '../assets/headset.svg?react';
+import userIcon from '../assets/user.svg?react';
+import logoutIcon from '../assets/right-from-bracket.svg?react';
+import type React from 'react';
 
 const linkBase =
   'flex gap-2 items-center block rounded px-3 py-2 text-base transition-colors duration-150';
@@ -23,28 +24,39 @@ const disabledClass = `${linkBase} text-[#B9C1CB] cursor-not-allowed`;
 const navHeaderClass =
   'mb-2 mt-2 px-3 text-[#B9C1CB] text-xs uppercase tracking-wide';
 const dividerClass = 'mx-1 border-b border-[#B9C1CB]';
+const activeIconClass = 'w-5 h-5 [&_*]:fill-black  text-black';
+const inactiveIconClass = 'w-5 h-5 text-[#CFD5DD] [&_*]:fill-[#CFD5DD]';
+
+const iconClass = (isActive: boolean) => {
+  return isActive ? activeIconClass : inactiveIconClass;
+};
+type IconType = React.ComponentType<React.SVGProps<SVGSVGElement>>;
 
 type NavItemProps = {
-  icon: string;
+  icon: IconType;
   label: string;
   to?: string;
   end?: boolean;
   disabled?: boolean;
 };
 
-const NavItem = ({ icon, label, to, end, disabled }: NavItemProps) => {
+const NavItem = ({ icon: Icon, label, to, end, disabled }: NavItemProps) => {
   if (disabled || !to) {
     return (
       <span className={disabledClass}>
-        <img src={icon} alt="" className="opacity-60" />
+        <Icon className={inactiveIconClass} />
         <span>{label}</span>
       </span>
     );
   } else {
     return (
-      <NavLink to={to} end={end} className={linkClass}>
-        <img src={icon} alt=""  />
-        <span>{label}</span>
+      <NavLink to={to} end={end}>
+        {({ isActive }) => (
+          <div className={linkClass({ isActive })}>
+            <Icon className={iconClass(isActive)} />
+            <span>{label}</span>
+          </div>
+        )}
       </NavLink>
     );
   }
